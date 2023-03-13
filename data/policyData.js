379afaff4710e86139e6c38353c21fd1145bb747
sebastian-lucjan <sebastian.lucjan@gmail.com>
2023-03-13 11:16:58 +0100

    Add bask regulations

---
+++ data/policyData.js
@@ -0,0 +1,76 @@
+const policyData = {
+  // Polityka Prywatności
+  //
+  // Zasady ogólne
+  //
+  // Polityka prywatności zawiera obowiązujące w Serwisie zasady zbierania i wykorzystywania danych o Użytkownikach przy składaniu przez Użytkowników zamówień na oferowane przez BASK towary i usługi.
+  //   Polityka prywatności dotyczy przetwarzania i ochrony danych osobowych Użytkowników w związku z korzystaniem z serwisu www.bask.com.pl, zwanego dalej „Serwisem”, w tym jego podstron, administrowanego przez BASK Aleksandra Surma, posiadającą numer identyfikacji podatkowej NIP: 9462529521, numer Regon: 522831174.
+  // Celem Polityki Prywatności jest zapewnienie Użytkownikom Serwisu ochrony prywatności spełniającej standardy określone w obowiązujących przepisach prawnych, w szczególności w ustawie z dnia 18 lipca 2002 r. o świadczeniu usług drogą elektroniczną (Dz. U. z 2002 r., Nr 144, poz. 1204 z późn. zm.), rozporządzeniu Parlamentu Europejskiego i Rady (UE) 2016/679 z 27 kwietnia 2016r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych) – RODO (Dz. Urz. UE L 119, s.1) oraz w ustawie z dnia 16 lipca 2004 r. Prawo telekomunikacyjne.
+  //   Administrator danych osobowych
+  // Administratorem Twoich danych osobowych w rozumieniu przepisów o ochronie danych osobowych jest BASK Aleksandra Surma, NIP: 9462529521, REGON: 522831174.  Cele, podstawy prawne oraz okres przetwarzania danych osobowych wskazane są odrębnie w stosunku do każdego celu przetwarzania danych (patrz: postanowienia poniżej zawierające szczegółowy opis różnych celów przetwarzania danych).  W związku z przetwarzaniem przez nas Twoich danych osobowych, przysługują Ci następujące uprawnienia:
+  //   prawo do żądania dostępu do swoich danych osobowych, ich sprostowania, usunięcia lub ograniczenia przetwarzania,
+  //   prawo do wniesienia sprzeciwu wobec przetwarzania,
+  //   prawo do przenoszenia danych,
+  //   prawo do cofnięcia zgody na przetwarzanie danych osobowych w określonym celu, jeżeli uprzednio wyraziłeś taką zgodę,
+  //   prawo do wniesienia skargi do organu nadzorczego w związku z przetwarzaniem przez nas danych osobowych.
+  //   Powyższe uprawnienia możesz realizować zgodnie z zasadami opisanymi w art. 16 – 21 RODO, kontaktując się z nami pod adresem kontakt@back.com.pl. Możemy odmówić Ci realizacji niektórych uprawnień spośród wskazanych powyżej w sytuacji, w której realizacja danego uprawnienia stałaby w sprzeczności z uzasadnionym celem przetwarzania przez nas danych. Przykładowo, możemy odmówić Ci usunięcia danych osobowych zawartych w złożonym przez Ciebie zamówieniu do czasu upływu terminu przedawnienia roszczeń z tytułu zawartej za pośrednictwem sklepu umowy.  Podanie przez Ciebie danych osobowych jest zawsze dobrowolne, ale niezbędne, by założyć konto w sklepie, złożyć zamówienie, zapisać się na newsletter, skontaktować z nami, odstąpić od umowy, złożyć reklamację lub dodać komentarz na blogu.  Gwarantujemy Ci poufność wszelkich przekazanych nam danych osobowych. Zapewniamy podjęcie wszelkich środków bezpieczeństwa i ochrony danych osobowych wymaganych przez przepisy wymienione na wstępie niniejszej polityki prywatności. Dane osobowe są gromadzone z należytą starannością i odpowiednio chronione przed dostępem do nich przez osoby do tego nieupoważnione.  Odbiorcami danych osobowych mogą być:
+  //
+  //   1.BASK Aleksandra Surma, jako administrator danych osobowych, a także:
+  //   2.podmioty uprawnione do uzyskania danych osobowych na podstawie bezwzględnie obowiązujących przepisów prawa Unii Europejskiej lub prawa polskiego np. sądy lub organy administracji publicznej, organy państwowe,
+  //   3.banki i podmioty zapewniające płatności elektroniczne w celu niezbędnym do wykonania umowy sprzedaży oraz w celu niezbędnym do obsługi płatności związanych z zawarciem i wykonaniem umowy sprzedaży,
+  //   4. podwykonawcy, dostawcy lub zleceniobiorcy działający na rzecz BASK Aleksandra Surma, celem produkcji, konfekcjonowania, pakowania i wysyłki produktów będących przedmiotem umowy sprzedaży,
+  //   5.producenci produktów sprzedawanych przez BASK Aleksandra Surma, wyłącznie w celu wykonania dostawy w modelu „Wysyłka od producenta”, w tym w celu przygotowania, pakowania i wysyłki produktów oraz obsługi procesu reklamacji,
+  //   6. podmioty świadczące usługi IT oraz dostarczające oprogramowanie informatyczne,
+  //   7. biura rachunkowe świadczące usługi finansowo-księgowe,
+  //   9. kancelarie prawne świadczące usługi doradztwa prawnego,
+  //   10. operatorzy pocztowi i podmioty świadczące usługi kurierskie;
+  // Ww. podmioty przetwarzają dane osobowe na podstawie umowy powierzenia przetwarzania danych osobowych zawartej z BASK Aleksandra Surma a także wyłącznie zgodnie z poleceniami BASK Aleksandra Surma.
+  //   Wszystkie podmioty, którym powierzamy przetwarzanie danych osobowych gwarantują stosowanie odpowiednich środków ochrony i bezpieczeństwa danych osobowych wymaganych przez przepisy prawa.	  Konto użytkownika.
+  //   Zakładając konto użytkownika w sklepie, musisz podać swoje podstawowe dane, takie jak adres e-mail, imię i nazwisko oraz hasło.	  Swoje dane przypisane do konta w każdej chwili możesz zmodyfikować z wykorzystaniem opcji dostępnych po zalogowaniu się do konta.	  Podstawą prawną przetwarzania Twoich danych osobowych zawartych w koncie użytkownika jest wykonanie umowy o prowadzenie konta w sklepie, którą zawierasz z administratorem na podstawie regulaminu sklepu.	  Twoje dane osobowe zawarte na koncie użytkownika są przetwarzane przez czas funkcjonowania konta, czyli przez czas trwania umowy o prowadzenie konta w sklepie, o której mowa powyżej. W każdej chwili możesz podjąć decyzję o usunięciu konta, co skutkować będzie również usunięciem twoich danych osobowych z bazy. Pamiętaj jednak, że usunięcie konta nie skutkuje usunięciem danych dotyczących złożonych za pośrednictwem sklepu zamówień	.  Składanie zamówienia.
+  //   Składając zamówienie za pośrednictwem sklepu, musisz podać swoje dane osobowe niezbędne do realizacji zamówienia, tj. imię i nazwisko, adres e-mail, numer telefonu i adres do dostawy zamówienia. Możesz również podać dodatkowe dane, takie jak informacje o prowadzonej działalności gospodarczej. Jeżeli podałeś/aś wcześniej takie dane w swoim koncie użytkownika, to dane te zostaną automatycznie podstawione do formularza zamówienia, przy czym zawsze masz możliwość ich modyfikacji.  Podstawą prawną przetwarzania Twoich danych osobowych zawartych w zamówieniu jest wykonanie umowy sprzedaży, którą zawierasz na podstawie regulaminu sklepu.  Dane podane w formularzu zamówienia zostaną zapisane w bazie sklepu i będą w niej przechowywane do czasu przedawnienia roszczeń z tytułu zawartej za pośrednictwem sklepu umowy sprzedaży. Każde zamówienie odnotowywane jest jako odrębna pozycja w bazie. Oprócz danych osobowych odnotowywane są również szczegóły zamówienia takie jak data złożenia zamówienia, przedmiot i wartość zamówienia, wybrany sposób dostawy i płatności.	  Faktury  Jeżeli w związku ze złożonym zamówieniem, wystawiamy Ci fakturę, to w tym celu przetwarzamy Twoje dane osobowe w zakresie niezbędnym do wystawienia faktury. Podstawą prawną przetwarzania Twoich danych w tym przypadku jest wypełnienie prawnego obowiązku wystawienia faktury. Ponadto, wszystkie wystawione faktury uwzględniane są w dokumentacji księgowej, w związku z czym Twoje dane będą przetwarzane w ramach tej dokumentacji przez okres czasu wymagany przez przepisy prawa.
+  //   Zwroty i reklamacje
+  // Jeżeli odstępujesz od umowy zawartej za pośrednictwem sklepu lub składasz reklamację dotyczącą zakupionych produktów, to przekazujesz nam dane zawarte w formularzu odstąpienia od umowy lub w formularzu reklamacyjnym, a my te dane przetwarzamy w celu realizacji procedury odstąpienia od umowy lub procedury reklamacyjnej.  Podstawą prawną przetwarzania Twoich danych jest w tym przypadku wypełnienie przez nas prawnych obowiązków związanych z procedurą odstąpienia od umowy lub procedurą reklamacyjną.	  Formularze odstąpienia od umowy oraz formularze reklamacyjne są przez nas archiwizowane. Fakt odstąpienia od umowy lub złożenia reklamacji odnotowywany jest również w  systemie sklepowym.	   Kontakt e-mailowy	  Kontaktując się z nami za pośrednictwem poczty elektronicznej, przekazujesz nam swój adres e-mail jako adres nadawcy wiadomości. Ponadto, w treści wiadomości możesz zawrzeć również inne dane osobowe.	  Podstawą prawną przetwarzania danych osobowych w takim przypadku jest Twoja zgoda wynikająca z zainicjowania z nami kontaktu.	  Twoje dane osobowe przekazywane nam w ramach kontaktu e-mailowego przetwarzane są wyłącznie w celu obsługi Twojego zapytania. Treść korespondencji może podlegać archiwizacji.	   Komentarze na blogu	  Dodając komentarz na blogu, musisz podać swoje imię oraz adres e-mail. Twoje imię zostanie opublikowane wraz z treścią komentarza na blogu.	  Podstawą prawną przetwarzania Twoich danych osobowych jest Twoja zgoda wynikająca z woli dodania komentarza.	  Twoje dane osobowe będą przetwarzane przez czas funkcjonowania bloga. W każdej chwili możesz podjąć decyzję o usunięciu komentarza, co skutkować będzie również usunięciem twoich danych osobowych z bazy.
+  //
+  //
+  //   Pliki cookies i inne technologie śledzące
+  //
+  //
+  // Czym są cookies i do czego służą podczas korzystania z naszej strony internetowej ?
+  //
+  //   Nasza strona wykorzystuje pliki cookies.
+  //   Cookies to niewielkie informacje tekstowe, przechowywane na Twoim urządzeniu końcowym (np. komputerze, tablecie, smartfonie), które mogą być odczytywane przez nasz system teleinformatyczny.
+  //
+  //   Cookies pozwalają nam:
+  //   zapewnić prawidłowe funkcjonowanie strony jakiego oczekujesz,
+  //   zapamiętać Twoje zalogowanie do konta użytkownika przy każdej wizycie na stronie,
+  //   zapamiętać Twoje ustawienia podczas i pomiędzy wizytami na stronie,
+  //   poprawić szybkość i bezpieczeństwo korzystania ze strony,
+  //   umożliwiać Ci korzystanie z funkcji społecznościowych,
+  //   ulepszać funkcje dostępne dla Ciebie na stronie,
+  //   korzystać z narzędzi analitycznych.
+  //   Więcej szczegółów znajdziesz poniżej.
+  //   Czy muszę wyrazić zgodę na cookies ?
+  //   Podczas pierwszej wizyty na stronie wyświetlana jest Ci informacja na temat stosowania plików cookies. Zaakceptowanie i zamknięcie tej informacji oznacza, że wyrażasz zgodę na wykorzystywanie plików cookies zgodnie z postanowieniami niniejszej polityki prywatności.
+  //   Zgodę zawsze możesz wycofać, usuwając pliki cookies oraz zmieniając ustawienia plików cookies w Twojej przeglądarce.
+  //
+  //   Pamiętaj jednak, że wyłączenie plików cookies może powodować trudności w korzystaniu ze strony stosującej cookies !	   Podział cookies
+  //
+  // Cookies własne	  Pliki cookies można podzielić na własne oraz pochodzące od podmiotów trzecich. Jeżeli chodzi o cookies własne, wykorzystujemy je w celu prawidłowego działania strony, a w szczególności w następujących celach:
+  //   prawidłowe działanie koszyka i procesu zamówienia,
+  //   określenie, czy jesteś zalogowany, czy nie,
+  //   zapamiętanie twojego zalogowania,
+  //   odnotowanie faktu akceptacji regulaminu.
+  //   Cookies podmiotów trzecich
+  // Nasz blog oraz sklep wykorzystuje funkcje zapewniane przez podmioty trzecie, co wiąże się z wykorzystywaniem plików cookies pochodzących od podmiotów trzecich. Wykorzystanie tego rodzaju plików cookies zostało opisane poniżej.
+  //   Funkcje społecznościowe cookies podmiotów trzecich	  Blog wyposażony jest w mechanizm pozwalający na udostępnianie treści w serwisach społecznościowych. Mechanizm ten wykorzystuje pliki cookies firmy Oracle dotyczące usługi AddThis.	  Na blogu dostępny jest likebox oraz instagram feed. W tym celu wykorzystywane są pliki cookies firmy Facebook oraz Instagram.
+  //
+  //   Jakie funkcje dodatkowe spełniają cookies ?
+  //
+  //   Analiza i statystyka
+  // Wykorzystujemy cookies do śledzenia statystyk strony, takich jak liczba osób odwiedzających, rodzaj systemu operacyjnego i przeglądarki internetowej wykorzystywanej do przeglądania strony, czas spędzony na stronie, odwiedzane podstrony etc.
+  //   Korzystamy w tym zakresie z Google Analytics. Zbierane w tym zakresie informacje są całkowicie anonimowe i nie pozwalają na Twoją identyfikację.	  Logi serwera	  Korzystanie ze strony wiąże się z przesyłaniem zapytań do serwera, na którym przechowywana jest strona. Każde zapytanie skierowane do serwera zapisywane jest w logach serwera.	  Logi obejmują m.in. Twój adres IP, datę i czas serwera, informacje o przeglądarce internetowej i systemie operacyjnym z jakiego korzystasz. Logi zapisywane i przechowywane są na serwerze.	  Dane zapisane w logach serwera nie są kojarzone z konkretnymi osobami korzystającymi ze strony i nie są wykorzystywane przez nas w celu Twojej identyfikacji.  Logi serwera stanowią wyłącznie materiał pomocniczy służący do administrowania stroną a ich zawartość nie jest ujawniana nikomu poza osobami upoważnionymi do administrowania serwerem.
+  //   W jaki sposób działa zautomatyzowane przetwarzanie danych osobowych czyli profilowanie ??
+  // Korzystamy z technologii przechowującej i uzyskującej dostęp do informacji na Twoim komputerze bądź innym urządzeniu podłączonym do sieci (w szczególności z wykorzystaniem plików cookies) dotyczących Twojej aktywności w Internecie, w celu prezentowania Ci dopasowanych reklam, oceny niektórych informacji o Tobie, również w ramach zautomatyzowanego przetwarzania danych osobowych, czyli profilowania (analizujemy Twoją aktywność dzięki czemu możemy lepiej dostosować się do określonych, ogólnych grup naszych klientów, nie wpływając jednak istotnie na ich decyzje – chyba, że wyrazisz na to odrębną zgodę), analiz rynkowych oraz statystycznych, oraz poprawy jakości prezentowanych Ci informacji. Technologią taką posługują się również nasi partnerzy, z którymi współpracujemy, którzy również mogą instalować takie pliki na Twoim urządzeniu podczas Twojego korzystania z naszych usług. Abyśmy wraz z naszymi partnerami mogli korzystać z tej technologii w celach związanych ze świadczeniem usług drogą elektroniczną, prosimy wyraź na to zgodę, np. przechodząc do naszego serwisu i umożliwiając nam takie działanie w ramach ustawień Twojej przeglądarki.
+};
+
+export default policyData;
--- pages/policy/index.jsx
+++ pages/policy/index.jsx
@@ -1,5 +1,4 @@
 import BaseLayout from 'components/BaseLayout';
-import Link from 'next/link';
 
 const title = '';
 const description = '';
@@ -11,24 +10,11 @@ export default function Policy() {
 
   return (
     <BaseLayout seoData={seoData}>
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 pb-16">
-        <div className="mt-16 w-full">
-          <div className="flex flex-col laptop:flex-row">
-            <div className="flex flex-col laptop:w-2/5 mr-16">
-              <div className="px-4 sm:px-0 tablet:px-6 w-full mb-12 bg-amber-100">
-                {/* todo: change styles/classes */}
-                <h3 className="text-4xl font-bold leading-6 text-gray-900">Polityka prywatności</h3>
-                <p className="mt-1 text-sm tablet:text-base tablet:leading-6 text-gray-700 py-8">
-                  Pragniemy podzielić się naszą wiedzą i doświadczeniem, które wykorzystujemy w dążeniu do zdrowia i piękna. Podstawową wartością
-                  naszej marki jest chęć dawania przyjemności ze stosowania naszych produktów.
-                </p>
-                <div className="hidden laptop:block">
-                  <Link href="/uv-education" as="/edukacja-uv">
-                    <p className="inline-block text-sm underline cursor-pointer">DOWIEDZ SIĘ WIĘCEJ...</p>
-                  </Link>
-                </div>
-              </div>
-            </div>
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 mb-12 mt-10">
+        <div className="flex flex-col">
+          <div className="px-4 sm:px-0 tablet:px-6 w-full ">
+            <h3 className="text-4xl font-bold leading-6 text-gray-900">Polityka prywatności</h3>
+            <p className="mt-1 text-sm tablet:text-base tablet:leading-6 text-gray-700 py-8">Już wkrótce...</p>
           </div>
         </div>
       </div>
