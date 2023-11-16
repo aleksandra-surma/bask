38ecb1ecc5f3a79ccf5934647d121197c1483abd
sebastian-lucjan <sebastian.lucjan@gmail.com>
2023-11-16 14:40:00 +0100

    Refactor: product added to basket window mobile view

--- components/ProductAdded/index.jsx
+++ components/ProductAdded/index.jsx
@@ -7,27 +7,25 @@ export default function ProductAdded({ addedProduct, setAddedProduct, selectedCo
 
   return (
     <div className="fixed top-0 z-10 flex h-screen w-screen items-center justify-center bg-[rgba(0,0,0,0.3)]">
-      <div className="mx-4 rounded-lg bg-white p-4 sm:p-8">
-        {/*<div className="sm:y-[unset] m-4 h-full w-full rounded-lg bg-white p-4 sm:w-[unset] sm:p-8">*/}
-
-        <div className="relative mb-8 flex border-b-2 pb-6">
-          <h2 className="tablet:6xl text-lg sm:text-3xl">Produkt został dodany do koszyka</h2>
+      <div className="m-[20px] max-h-[100vh-40px] rounded-lg bg-white p-4 sm:p-8">
+        <div className="relative mb-4 flex border-b-2 pb-3 sm:mb-8 sm:pb-6">
+          <h2 className="tablet:6xl max-w-[90%] text-lg sm:text-2xl">Produkt został dodany do koszyka</h2>
 
           <button type="button" onClick={() => setAddedProduct(null)} className="absolute right-0 text-lg font-bold sm:right-[-20px] sm:top-[-20px]">
             X
           </button>
         </div>
         <div className="flex flex-col">
-          <div className="relative h-[210px] w-[120px] sm:h-[280px] sm:w-[160px]">
+          <div className="relative h-[180px] w-[90px] sm:h-[280px] sm:w-[160px] xs:h-[210px] xs:w-[120px]">
             <Image src={imgSrc} className="rounded-xl" layout="fill" objectFit="cover" />
           </div>
-          <div className="mx-auto justify-center pt-6 text-lg sm:text-2xl">
-            <h3 className="font-semibold">{addedProduct.name}</h3>
-            <div className="my-6 flex">
+          <div className="mx-auto justify-center pt-4 text-lg sm:text-2xl xs:pt-6">
+            <h3 className="text-lg font-semibold sm:text-2xl">{addedProduct.name}</h3>
+            <div className="my-4 flex xs:my-6">
               <p className="mr-4">Rozmiar:</p>
               <p className="font-semibold">{selectedSize}</p>
             </div>
-            <div className="my-6 flex">
+            <div className="my-4 flex xs:my-6">
               <p className="mr-4">Kolor:</p>
               {/* <p className="text-xl font-semibold">{selectedColor}</p> */}
               <span
@@ -38,21 +36,21 @@ export default function ProductAdded({ addedProduct, setAddedProduct, selectedCo
                 )}
               />
             </div>
-            <div className="mt-6 flex sm:my-6">
+            <div className="mt-4 flex sm:my-6 xs:mt-6">
               <p className="mr-4">Cena:</p>
 
               {addedProduct.promotionValue > 0 ? (
-                <p className="text-2xl font-semibold">{addedProduct.promotionPrice} zł</p>
+                <p className="text-xl font-semibold xs:text-2xl">{addedProduct.promotionPrice} zł</p>
               ) : (
-                <p className="text-2xl font-semibold">{addedProduct.price} zł</p>
+                <p className="text-xl font-semibold xs:text-2xl">{addedProduct.price} zł</p>
               )}
             </div>
-            <div className="flex justify-between">
-              <button type="button" className="text-md underline underline-offset-4 sm:text-2xl" onClick={() => setAddedProduct(null)}>
+            <div className="relative mt-2 flex w-[100%] justify-between">
+              <button type="button" className="w-[45%] text-base underline underline-offset-4 sm:text-2xl" onClick={() => setAddedProduct(null)}>
                 Kontynuj zakupy
               </button>
               <Link href="/card-summary" as="/koszyk">
-                <a className="text-md cursor-pointer p-4 underline underline-offset-4 underline-offset-4 transition hover:text-black hover:underline sm:mr-6 sm:text-2xl tablet:ml-8 tablet:mr-8 tablet:p-0">
+                <a className="w-[45%] cursor-pointer text-center text-base underline underline-offset-4 underline-offset-4 transition hover:text-black hover:underline sm:mr-6 sm:text-2xl tablet:ml-8 tablet:mr-8 tablet:p-0">
                   Przejdź do koszyka
                 </a>
               </Link>
